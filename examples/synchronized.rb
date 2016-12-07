fe057bfb1c721b3a48c0efbd4755533195cac23d
mjago <martyn.jago@btinternet.com>
2016-12-07 22:04:10 +0000

    update synchronised.rb

--- examples/synchronized.rb
+++ examples/synchronized.rb
@@ -4,23 +4,21 @@ require_relative 'rn4020'
 SERVER_MAC_ADDR = '001EC03E38ED'.freeze
 PC_UUID =        '123456789012345678901234567890FF'.freeze
 PS_UUID_PREFIX = '1234567890123456789012345678904'.freeze # final digit added later!
-
 Mode = Struct.new(:port, :baud, :ss, :sr)
-
 Service = Struct.new(:pc_uuid, :ps_uuid_prefix, :ps_count, :ps_attribs, :ps_size) do
   def ps_size_to_s
     '0' << ps_size.to_s
   end
 end
-
 @bad_reads = 0
 @count = 0
+@debug_count = 0
 @reading = 0
 @suppress_verbose_output = true
 @suppress_write_info = false
 @sensor_count = 5
 @read_buf = ''
-
+@service = Service.new(PC_UUID, PS_UUID_PREFIX, 5, '02', 4)
 @chn = { client: Mode.new('/dev/cu.usbmodem1411',
                           115_200,
                           '00000000',
@@ -29,22 +27,27 @@ end
                           19_200,
                           '00000001',
                           '00060000') }
-
-@rn_client = RN4020.new(@chn[:client].port, (@chn[:client].baud))
-@rn_server = RN4020.new(@chn[:server].port, (@chn[:server].baud))
-@service = Service.new(PC_UUID, PS_UUID_PREFIX, 5, '02', 4)
+@rn_client = RN4020.new('Client', @chn[:client].port, (@chn[:client].baud))
+@rn_server = RN4020.new('Server', @chn[:server].port, (@chn[:server].baud))
 
 #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
 
-def special_puts(x, y)
-  return if @suppress_verbose_output
-  return if y.strip.empty?
-  print "#{x}: "
-  y.split("\n").each_with_index do |line,idx|
-    puts line if(idx == 0)
-    puts "        " + line unless(idx == 0)
+def debug_puts(x)
+  if @debug
+    puts x
   end
-  puts
+end
+
+def check_comms
+  loop do
+    break if @rn_client.dump
+    sleep 0.1
+  end
+
+  loop do
+    break if @rn_server.dump
+    sleep 0.1
+  end
 end
 
 def each_mode
@@ -62,111 +65,194 @@ def formatted_reading
 end
 
 def reset
-  puts 'Error Locked up!'
-  puts 'Waiting'
+  debug_puts 'Error Locked up!'
+  debug_puts 'Waiting'
   each_mode { |md| md.write('K') }
-  sleep 2
-  each_mode { |md| md.flush }
+  each_mode(&:flush)
 
-  puts 'Retrying...'
+  debug_puts 'Retrying...'
   @bad_reads += 1
 end
 
-def run_test
-  each_mode { |md| md.flush }
-  each_mode { |md| md.full_reset }
+def initial_bond
+  debug_puts 'full reset'
+  each_mode(&:flush)
+  each_mode(&:full_reset)
 
-  exit unless @rn_client.server_services(@chn[:client].ss)
-  exit unless @rn_server.server_services(@chn[:server].ss)
-  exit unless @rn_client.options(@chn[:client].sr)
-  exit unless @rn_server.options(@chn[:server].sr)
-  exit unless @rn_client.clean_private_services
-  exit unless @rn_server.clean_private_services
-  exit unless @rn_server.ps(@service.pc_uuid)
+  debug_puts 'setting services'
+  return false  unless @rn_client.server_services(@chn[:client].ss)
+  return false  unless @rn_server.server_services(@chn[:server].ss)
+  return false  unless @rn_client.options(@chn[:client].sr)
+  return false  unless @rn_server.options(@chn[:server].sr)
+  return false  unless @rn_client.clean_private_services
+  return false  unless @rn_server.clean_private_services
+  return false  unless @rn_server.ps(@service.pc_uuid)
 
   @service.ps_count.times do |count|
-    exit unless @rn_server.pc(@service.ps_uuid_prefix + count.to_s,
+    return false  unless @rn_server.pc(@service.ps_uuid_prefix + count.to_s,
                               @service.ps_attribs,
                               @service.ps_size_to_s)
   end
 
-  each_mode { |md| exit unless md.reboot }
+  debug_puts 'reboot'
 
-  exit unless @rn_server.advertise
+  each_mode { |md| return false  unless md.reboot }
+
+  debug_puts 'advertise'
+
+  return false  unless @rn_server.advertise
+
+  debug_puts 'scan'
 
   if @rn_client.scan
     @server_addr = @rn_client.found[0..-3]
     p @server_addr
-    exit unless @rn_client.stop_scan
-    exit unless @rn_client.connect(@server_addr)
-    exit unless @rn_client.bond
 
-    exit unless @rn_server.secure
+    debug_puts 'stop scan'
+
+    return false  unless @rn_client.stop_scan
+
+    debug_puts 'connect'
+
+    return false  unless @rn_client.connect(@server_addr)
+
+    debug_puts 'bond'
+
+    return false  unless @rn_client.bond
+
+    debug_puts 'secure'
+
+    return false  unless @rn_server.secure
+
+    debug_puts 'list'
 
     each_mode do |md|
-      exit unless md.list_client_services
-      exit unless md.list_server_services
+      return false  unless md.list_client_services
+      return false  unless md.list_server_services
     end
 
+    debug_puts 'dump'
+
     each_mode do |md|
-      exit unless md.dump
+      return false  unless md.dump
     end
-    exit unless @rn_client.disconnect
+    return false  unless @rn_client.disconnect
   end
 
   @suppress_write_info = true
 
-  if @rn_client.bonded
-    puts "bonded = #{@rn_client.bonded}"
-    loop do
-      loop do
-        puts "\ncount: #{@count}"
-        puts "bad_reads: #{@bad_reads}" if @bad_reads > 0
+  each_mode do |md|
+    return false  unless md.dump
+  end
 
-        each_sensor do |sn|
-          break unless @rn_server.write_reading(PS_UUID_PREFIX + sn,
-                                                formatted_reading)
-        end
+  debug_puts 'is client bonded?'
 
-        @rn_client.reconnect
-        break unless @rn_server.advertise
-        break unless @rn_client.wait_for_connection
+  bonded = @rn_client.bonded
+  return false unless bonded
+  debug_puts 'yes'
+  debug_puts "bonded to #{bonded}"
+  bonded
+end
 
-        break unless @rn_client.dump if(@count % 32 == 8)
-        puts "bta       = #{@rn_client.bta       }" if(@count % 32 == 8)
-        puts "name      = #{@rn_client.name      }" if(@count % 32 == 8)
-        puts "role      = #{@rn_client.role      }" if(@count % 32 == 8)
-        puts "connected = #{@rn_client.connected }" if(@count % 32 == 8)
-        puts "bonded    = #{@rn_client.bonded    }" if(@count % 32 == 8)
-        puts "ss        = #{@rn_client.ss        }" if(@count % 32 == 8)
+def exchange_values
+  loop do
+    puts "\ncount: #{@count}"
+    debug_puts "bad_reads: #{@bad_reads}" if @bad_reads > 0
+    debug_puts 'writing readings?'
 
-        break unless @rn_server.dump if(@count % 32 == 24)
-        puts "bta       = #{@rn_server.bta       }" if(@count % 32 == 24)
-        puts "name      = #{@rn_server.name      }" if(@count % 32 == 24)
-        puts "role      = #{@rn_server.role      }" if(@count % 32 == 24)
-        puts "connected = #{@rn_server.connected }" if(@count % 32 == 24)
-        puts "bonded    = #{@rn_server.bonded    }" if(@count % 32 == 24)
-        puts "ss        = #{@rn_server.ss        }" if(@count % 32 == 24)
-
-        # client read reading
-        each_sensor do |sn|
-          value = @rn_client.read_value(PS_UUID_PREFIX + sn)
-          puts value
-          break unless value == formatted_reading
-        end
-
-        break unless @rn_client.disconnect
-        @count += 1
-        @reading += 1
-        @reading = 0 if @reading > 65535
-      end
-
-      reset
+    @rn_server.wake
+    each_sensor do |sn|
+      break unless @rn_server.write_reading(PS_UUID_PREFIX + sn,
+                                            formatted_reading)
     end
+
+    debug_puts 'reconnecting'
+
+    @rn_client.reconnect
+
+    sleep 0.05
+
+    @debug_count += 1
+    if(@debug_count % 64 == 34) ###   #   #   #   #   #   #   #   #   #   #
+      puts 'breaking'
+      return false
+    end
+
+    debug_puts 'advertising to connect'
+
+    break unless @rn_server.advertise_to_connect
+
+    debug_puts 'waiting for connection'
+    break unless @rn_client.wait_for_connection
+
+    if(@count % 4 == 0)
+      break unless @rn_client.dump
+      puts "client bta       = #{@rn_client.bta       }"
+      puts "client name      = #{@rn_client.name      }"
+      puts "client role      = #{@rn_client.role      }"
+      puts "client connected = #{@rn_client.connected }"
+      puts "client bonded    = #{@rn_client.bonded    }"
+      puts "client ss        = #{@rn_client.ss        }"
+    end
+
+    if(@count % 4 == 2)
+      break unless @rn_server.dump
+      puts "server bta       = #{@rn_server.bta       }"
+      puts "server name      = #{@rn_server.name      }"
+      puts "server role      = #{@rn_server.role      }"
+      puts "server connected = #{@rn_server.connected }"
+      puts "server bonded    = #{@rn_server.bonded    }"
+      puts "server ss        = #{@rn_server.ss        }"
+    end
+
+    debug_puts 'reading values'
+
+    if (@debug_count % 64 == 6) ###   #   #   #   #   #   #   #   #   #   #
+      puts 'breaking'
+      return false
+    end
+
+    # client read reading
+    each_sensor do |sn|
+      value = @rn_client.read_value(PS_UUID_PREFIX + sn)
+      puts "client read #{value}"
+      break unless value == formatted_reading
+    end
+
+    debug_puts 'disconnecting'
+
+    break unless @rn_client.disconnect
+    break unless @rn_server.wait_for_disconnect
+    @count += 1
+    @reading += 1
+    @reading = 0 if @reading > 65_535
+
+#    debug_puts 'go dormant'
+#    @rn_server.go_dormant
+    return true
+  end
+  false
+end
+
+def run_test
+  @peripheral = ''
+  loop do
+    @peripheral = initial_bond
+    break unless @peripheral.empty?
+    debug_puts 'retrying'
+    unbond
+  end
+  loop do
+    next if exchange_values
+    @rn_client.disconnect
+    @rn_server.wait_for_disconnect
+    @rn_server.go_dormant
+    sleep 2
   end
 end
 
-def exit
+def unbond
+  debug_puts 'unbond'
   each_mode do |md|
     next unless md.unbond
     md.read
@@ -175,5 +261,6 @@ end
 
 ### Start
 
+check_comms
 run_test
-exit
+
